253b4653db72ecbe8d5c773db489aa300dfb20cc
89deeksha <deekshajain0310@gmail.com>
2025-03-07 14:49:18 +0530

    loginadded

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
+import { useAuth0 } from "@auth0/auth0-react";
 function Navbar() {
+  const { loginWithRedirect,isAuthenticated,logout,user } = useAuth0();
   return (
     <div>
         
@@ -25,6 +26,26 @@ function Navbar() {
         <li className="nav-item">
           <Link className="nav-link text-white" to="/contact">Contact</Link>
         </li>
+
+
+<li>{
+  isAuthenticated && <p>{user.name}</p>
+}</li>
+
+
+
+{
+isAuthenticated?<li><button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+Log Out
+</button></li>:<li><button onClick={() => loginWithRedirect()}>Log In</button></li>
+
+}
+
+
+
+
+        
+        
         {/* <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown link
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Auth0Provider
+    domain="dev-ub6d748ptyawtoug.us.auth0.com"
+    clientId="cGSDpyYZoYqexQ17nK9ZRaLI1rolXT1j"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </StrictMode>,
+    </Auth0Provider>,
+ 
 )
